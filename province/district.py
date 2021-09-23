5b84e92387712df36a4900ae731912c06766cfb0
Ali Poursaeid <you@example.com>
2021-09-23 19:02:46 +0330

    feat: new method to access district detaile by farsi key

--- province/district.py
+++ province/district.py
@@ -17,3 +17,11 @@ def towns_list_by_district_key(district_obj: str):
             for i in data[key].keys():
                 key_list.append(i)
     return key_list
+
+
+def search_by_fa_key(fa_key: str):
+    data = get_data(SETTINGS['districts_path'])
+    for key in data:
+        if key == fa_key:
+            return data[fa_key]
+    return {}
